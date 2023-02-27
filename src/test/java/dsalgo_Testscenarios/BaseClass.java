51ece8c4c577b2e4d2fff14538d45c6d276de316
Poojajsp <126360361+Poojajsp@users.noreply.github.com>
2023-02-27 00:37:00 -0500

    remove comment

--- src/test/java/dsalgo_Testscenarios/BaseClass.java
+++ src/test/java/dsalgo_Testscenarios/BaseClass.java
@@ -93,7 +93,7 @@ public void SS(WebDriver driver, String tname)
 	}
 
 public static String timestamp() {
-	//Comment
+	
 	
 	return new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(new Date());
 }
